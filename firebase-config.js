f04f41b4ebc8155c05a924491a851406912fe05c
dinguspingus84acl <jack@atcostleads.com>
2026-08-02 21:50:42 -0400

    Add Firebase config placeholder for live draft rooms

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "PASTE_FIREBASE_API_KEY",
+  authDomain: "PASTE_FIREBASE_AUTH_DOMAIN",
+  databaseURL: "PASTE_FIREBASE_DATABASE_URL",
+  projectId: "PASTE_FIREBASE_PROJECT_ID",
+  storageBucket: "PASTE_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "PASTE_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "PASTE_FIREBASE_APP_ID"
+};
